abcabf5500b3aefea39158369d9a9c61398e6aae
jaruu <158283099+zarszs@users.noreply.github.com>
2024-03-02 02:23:42 +0700

    Update __main__.py

--- rams/__main__.py
+++ rams/__main__.py
@@ -26,7 +26,7 @@ try:
     ).json()
     if user.id in ramblacklist:
         LOGS.warning(
-            "rams TIDAK DAPAT BERJALAN, KARNA LO KONTOL MAKE SEMEMA MENA, BOT LO DI MATIIN HEHEH, LAPORKAN KE @UTITS"        )
+            "UBOT TIDAK DAPAT BERJALAN, KARNA LO KONTOL MAKE SEMEMA MENA, BOT LO DI MATIIN HEHEH, LAPORKAN KE @UTITS"        )
         sys.exit(1)
    # if 1826643972 not in DEVS:
       #  LOGS.warning(
@@ -40,7 +40,7 @@ except Exception as e:
 
 LOGS.info(f"Total Clients = {total} User")
 LOGS.info(f"Jika {user.first_name} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/jarsuprot")
-LOGS.info(f"✨ JAR - UBOT ✨ v {BOT_VER} [DAH AKTIF NGENTOT!!!]")
+LOGS.info(f"✨ JAR - UBOT ✨ v {BOT_VER} [SUDAH AKTIF YEEYYY!!!]")
    
 
 LOOP.run_until_complete(ram_ubot_on())
