fadf34b62281251f254aa8378d41f11d0336bd24
jaruu <158283099+zarszs@users.noreply.github.com>
2024-03-01 09:46:40 +0700

    Update ramwords.py

--- rams/modules/ramwords.py
+++ rams/modules/ramwords.py
@@ -11,176 +11,6 @@ from session.pyrogRam import *
 
 eor = edit_or_reply
 
-
-@ram_cmd(pattern="aa(?: |$)(.*)")
-async def _(e):
-    txt = random.choice(ROAS)
-    await eor(e, txt)
-    
-@ram_cmd(pattern="ee(?: |$)(.*)")
-async def _(e):
-    txt = random.choice(SUKASUKA)
-    await eor(e, txt)
-
-@ram_cmd(pattern="ab(?: |$)(.*)")
-async def _(e):
-    txt = random.choice(PAKBOY)
-    await eor(e, txt)
-    
-@ram_cmd(pattern="eb(?: |$)(.*)")
-async def _(e):
-    txt = random.choice(GENIT)
-    await eor(e, txt)
-    
-@ram_cmd(pattern="a(?: |$)(.*)")
-async def _(e):
-    txt = ram_a
-    await eor(e, txt)
-    
-    
-@ram_cmd(pattern="bb(?: |$)(.*)")
-async def _(e):
-    txt = ram_b
-    await eor(e, txt)    
-    
-@ram_cmd(pattern="cc(?: |$)(.*)")
-async def _(e):
-    txt = ram_c
-    await eor(e, txt)
-    
-@ram_cmd(pattern="dd(?: |$)(.*)")
-async def _(e):
-    txt = ram_d
-    await eor(e, txt)
-    
-    
-@ram_cmd(pattern="ee(?: |$)(.*)")
-async def _(e):
-    txt = ram_e
-    await eor(e, txt)    
-    
-    
-@ram_cmd(pattern="ff(?: |$)(.*)")
-async def _(e):
-    txt = ram_f
-    await eor(e, txt)
-    
-@ram_cmd(pattern="gg(?: |$)(.*)")
-async def _(e):
-    txt = ram_g
-    await eor(e, txt)
-    
-    
-@ram_cmd(pattern="hh(?: |$)(.*)")
-async def _(e):
-    txt = ram_h
-    await eor(e, txt)    
-    
-@ram_cmd(pattern="ii(?: |$)(.*)")
-async def _(e):
-    txt = ram_i
-    await eor(e, txt)
-    
-@ram_cmd(pattern="jj(?: |$)(.*)")
-async def _(e):
-    txt = ram_j
-    await eor(e, txt)
-    
-    
-@ram_cmd(pattern="kk(?: |$)(.*)")
-async def _(e):
-    txt = ram_k
-    await eor(e, txt)    
-    
-    
-@ram_cmd(pattern="ll(?: |$)(.*)")
-async def _(e):
-    txt = ram_l
-    await eor(e, txt)
-    
-    
-@ram_cmd(pattern="m(?: |$)(.*)")
-async def _(e):
-    txt = ram_m
-    await eor(e, txt)
-    
-    
-@ram_cmd(pattern="n(?: |$)(.*)")
-async def _(e):
-    txt = ram_n
-    await eor(e, txt)    
-    
-@ram_cmd(pattern="o(?: |$)(.*)")
-async def _(e):
-    txt = ram_o
-    await eor(e, txt)
-    
-@ram_cmd(pattern="pp(?: |$)(.*)")
-async def _(e):
-    txt = ram_p
-    await eor(e, txt)
-    
-    
-@ram_cmd(pattern="qq(?: |$)(.*)")
-async def _(e):
-    txt = ram_q
-    await eor(e, txt)    
-    
-    
-@ram_cmd(pattern="r(?: |$)(.*)")
-async def _(e):
-    txt = ram_r
-    await eor(e, txt)
-    
-@ram_cmd(pattern="s(?: |$)(.*)")
-async def _(e):
-    txt = ram_s
-    await eor(e, txt)
-    
-    
-@ram_cmd(pattern="t(?: |$)(.*)")
-async def _(e):
-    txt = ram_t
-    await eor(e, txt)    
-    
-@ram_cmd(pattern="u(?: |$)(.*)")
-async def _(e):
-    txt = ram_u
-    await eor(e, txt)
-    
-@ram_cmd(pattern="v(?: |$)(.*)")
-async def _(e):
-    txt = ram_v
-    await eor(e, txt)
-    
-    
-@ram_cmd(pattern="w(?: |$)(.*)")
-async def _(e):
-    txt = ram_w
-    await eor(e, txt)    
-    
-    
-@ram_cmd(pattern="x(?: |$)(.*)")
-async def _(e):
-    txt = ram_x
-    await eor(e, txt)
-    
-@ram_cmd(pattern="y(?: |$)(.*)")
-async def _(e):
-    txt = ram_y
-    await eor(e, txt)   
-
-@ram_cmd(pattern="z(?: |$)(.*)")
-async def _(e):
-    txt = ram_z
-    await eor(e, txt)   
-
-
-####### end of line #######
-# ini ibu budi #
-# yang dihamili bapak budi #
-
-
 @ram_cmd(pattern="1(?: |$)(.*)")
 async def _(e):
     txt = ram_ab
@@ -286,7 +116,7 @@ async def _(e):
     txt = ram_tu
     await eor(e, txt)
 
-ram_cmd(pattern="20(?: |$)(.*)")
+@ram_cmd(pattern="20(?: |$)(.*)")
 async def _(e):
     txt = ram_uv
     await eor(e, txt)    
@@ -322,13 +152,9 @@ async def _(e):
 
 CMD_HELP.update(
     {
-        "ramwords": f"**Plugin : **`ramwords`\
-        \n\n  𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `{cmd}a sampai {cmd}z`\
-        \n  ❍▸ : **kata kata war.\
+        "ramwords": f"**Plugin : **`gombal`\
         \n\n  𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `{cmd}1 sampai {cmd}25\
         \n  ❍▸ : **kata kata gombalan.\
-        \n\n  𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `{cmd}aa atau {cmd}ee\
-        \n  ❍▸ : **ramdom kalimat war.\
         \n\n  𝘾𝙤𝙢𝙢𝙖𝙣𝙙 :** `{cmd}ab atau {cmd}eb`\
         \n  ❍▸ :** random kalimat gombal.\
        "
