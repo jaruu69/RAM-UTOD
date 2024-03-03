ceefce5305d3b3c50f73924ac25f4c4ffa1ab808
jaruu <158283099+zarszs@users.noreply.github.com>
2024-03-03 13:24:22 +0700

    Update utils.py

--- rams/utils/utils.py
+++ rams/utils/utils.py
@@ -95,7 +95,7 @@ async def autobot():
     await bot.send_read_acknowledge("botfather")
     if isdone.startswith("Sorry,"):
         ran = randint(1, 100)
-        username = "Asisstant" + (str(who.id))[6:] + str(ran) + "Bot"
+        username = who.first_name + (str(who.id))[6:] + str(ran) + "bot"
         await bot.send_message(bf, username)
         await asyncio.sleep(1)
         nowdone = (await bot.get_messages(bf, limit=1))[0].text
